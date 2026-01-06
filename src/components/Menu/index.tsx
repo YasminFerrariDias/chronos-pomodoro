c67e8fc17b3733890b09471dd987d57fca1d65bf
Yasmin <yasminferraridias@gmail.com>
2026-01-06 11:25:53 -0300

    Capturando o efeito de clique.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,35 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    console.log('Clicado', Date.now());
+  }
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink}>
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} aria-label="Ir para a Home!" title="Ir para a Home!">
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink}>
+      <a className={styles.menuLink} aria-label="Ver Histórico!" title="Ver Histórico!">
         <HistoryIcon />
       </a>
-      
-      <a className={styles.menuLink}>
+
+      <a className={styles.menuLink} aria-label="Ir para Configurações!" title="Ir para Configurações!">
         <SettingsIcon />
       </a>
-      
-      <a className={styles.menuLink}>
+
+      <a className={styles.menuLink} aria-label="Mudar Tema!" title="Mudar Tema!" onClick={handleThemeChange}>
         <SunIcon />
       </a>
     </nav>  
