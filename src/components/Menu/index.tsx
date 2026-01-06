8cbcbb633095c978d59d4dacfe6bc4af5923e5c4
Yasmin <yasminferraridias@gmail.com>
2026-01-06 18:15:35 -0300

    useEffect - hook

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -10,18 +10,35 @@ export function Menu() {
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) {
-    event.preventDefault();
+    event.preventDefault(); // não segue o link
+
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
-
-    // document.documentElement.setAttribute('data-theme', theme);
   }
+
+  /* useEffect(() => {
+    console.log('useEffect sem dependências', Date.now());
+  }); */
+  // executado toda vez que o componente renderiza na tela
+
+  /* useEffect(() => {
+    console.log('useEffect com array deps vazio', Date.now());
+  }, []); */
+  // executa apenas quando o React monta o componente na tela principal pela primeira vez -> util para buscar dados de API
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log("Olha, este componente será atualizado"); // limpa a sujeira que pode ter deixado na tela
+    };
+  }, [theme]);
+  // executa apenas quando o valor de theme mudou
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a className={styles.menuLink} aria-label="Ir para a Home!" title="Ir para a Home!">
         <HouseIcon />
       </a>
