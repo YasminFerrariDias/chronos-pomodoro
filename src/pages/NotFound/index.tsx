1f1fc4a65c31740704739ecb9c9c831747eaf5bd
Yasmin <yasminferraridias@gmail.com>
2026-01-06 19:01:06 -0300

    criação home e template

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../template/MainTemplate";
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem temporibus, enim vel suscipit distinctio, ea aliquam laborum hic itaque assumenda nihil! Sint doloremque minus nobis nemo, voluptates rem repudiandae.</p> 
+      </Container>
+     
+    </MainTemplate>
+  )
+}
