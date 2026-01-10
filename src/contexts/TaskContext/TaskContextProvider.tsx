491957d9cf1a90a2daec0077dcf8fdc5cc4dbc30
Yasmin <yasminferraridias@gmail.com>
2026-01-10 11:43:27 -0300

    APRENDENDO useREDUCER

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,13 +9,31 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(()=>{
-    console.log(state);
-  }, [state]);
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+    switch (action) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      case 'ZERAR':
+        return 0;
+    }
+
+    return state;
+  }, 0);
+
+  //useEffect(()=>{
+  //  console.log(state);
+  // }, [state]);
   
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>O número é {numero}</h1>
+      <button onClick={() => dispatch('INCREMENT')}>incremente</button>
+      <button onClick={() => dispatch('DECREMENT')}>decremente</button>
+      <button onClick={() => dispatch('ZERAR')}>zerar</button>
     </TaskContext.Provider>
   );
 }
