509c96bfb230a6597c2b1e26341bed5c03a7e6ce
Yasmin <yasminferraridias@gmail.com>
2026-01-10 12:18:17 -0300

    CÓDIGO LIMPO - sem o exemplo

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,47 +9,13 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  type ActionType = {
-    type: string;
-    payload?: number;
-  }
-
-  const [myState, dispatch] = useReducer((state, action) => {
-    console.log(state, action);
-
-    switch (action.type) {
-      case 'INCREMENT':{
-        if(!action.payload) return state;
-        return {
-          ...state,
-          secondsReamining: state.secondsReamining + action.payload,
-        }
-      }
-      case 'DECREMENT':{
-        if(!action.payload) return state;
-        return {
-          ...state,
-          secondsReamining: state.secondsReamining - action.payload,
-        }
-      }
-      case 'ZERAR':{
-        return {
-          secondsReamining: 0
-        }
-      }
-    }
-
-    return state;
-  }, {
-    secondsReamining: 0,
-  });
+  useEffect(()=>{
+    console.log(state);
+  }, [state]);
   
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <h1>O número é {JSON.stringify(myState)}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10})}>incremente + 10</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 20})}>DECREMENT - 20</button>
-      <button onClick={() => dispatch({ type: 'ZERAR', payload: 20})}>ZERAR</button>
+      {children}
     </TaskContext.Provider>
   );
 }
