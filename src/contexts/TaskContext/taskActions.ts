3bb5188cbe07078a389cf6340502ccb42d8ce93e
Yasmin <yasminferraridias@gmail.com>
2026-02-08 14:50:18 -0300

    Atualizando o contador a cada segundo no estado e na página

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -4,18 +4,25 @@ export const TaskActionTypes = {
   START_TASK: 'START_TASK',
   INTERRUPT_TASK: 'INTERRUPT_TASK',
   RESET_STATE: 'RESET_STATE',
+  COUNT_DOWN: 'COUNT_DOWN',
+  COMPLETED_TASK: 'COMPLETED_TASK',
 } as const;
 
 export type TaskActionsWithPayoload = 
-| {
-  type: typeof TaskActionTypes.START_TASK,
-  payload: TaskModel;
-};
+  | {
+    type: typeof TaskActionTypes.START_TASK;
+    payload: TaskModel;
+  } | {
+    type: typeof TaskActionTypes.COUNT_DOWN;
+    payload: {secondsRemaining: number};
+  }; 
 
 export type TaskActionsWithoutPayoload = | {
   type: typeof TaskActionTypes.RESET_STATE;
 } | {
   type: typeof TaskActionTypes.INTERRUPT_TASK;
+} | {
+  type: typeof TaskActionTypes.COMPLETED_TASK;
 };
 
 export type TaskActionModel = 
