81dd16f35ffe0807148ca023c829f9a2dae714b6
Yasmin <yasminferraridias@gmail.com>
2026-03-03 15:30:41 -0300

    Validando os campos do formulário antes de salvar os dados

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -7,6 +7,7 @@ import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 import { useRef } from "react";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { showMessage } from "../../adapters/showMessage";
 
 export type HomeProps = {
   state: TaskStateModel;
@@ -21,14 +22,49 @@ export function Settings() {
 
   function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    showMessage.dissmiss();
+
+    const formErrors: string[] = [];
 
     // quando usamos o ref precisamos acessar a propriedade current para obter o elemento DOM e depois acessar o valor do input
-    const workTime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
-    alert(
-      `Configurações salvas:\nFoco: ${workTime} minutos\nDescanso Curto: ${shortBreakTime} minutos\nDescanso Longo: ${longBreakTime} minutos`,
-    );
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime)) {
+      formErrors.push("O tempo de foco deve ser um número válido.");
+      return;
+    }
+
+    if (isNaN(shortBreakTime)) {
+      formErrors.push("O tempo de descanso curto deve ser um número válido.");
+      return;
+    }
+
+    if (isNaN(longBreakTime)) {
+      formErrors.push("O tempo de descanso longo deve ser um número válido.");
+      return;
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push("O tempo de foco deve ser entre 1 e 99 minutos.");
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push("O tempo de descanso curto deve ser entre 1 e 30 minutos.");
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push("O tempo de descanso longo deve ser entre 1 e 60 minutos.");
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach((error) => {
+        showMessage.error(error);
+      });
+      return;
+    }
+    alert("Configurações salvas com sucesso!");
   }
 
   return (
@@ -41,6 +77,8 @@ export function Settings() {
           Modifique as configurações para tempo de foco,
           <br />
           descanço curto e descanço longo.
+          <br />
+          OBSERVAÇÃO: Digite apenas números.
         </p>
       </Container>
 
