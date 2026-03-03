b482e57241ad7daaf3e4e74e160dfac59f0addf1
Yasmin <yasminferraridias@gmail.com>
2026-03-03 15:07:07 -0300

    Capturando o onSubmit (envio) do formulário com ref e useRef dos inputs

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -5,45 +5,86 @@ import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export type HomeProps = {
-  state: TaskStateModel,
+  state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
-}
+};
 
 export function Settings() {
-  
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // quando usamos o ref precisamos acessar a propriedade current para obter o elemento DOM e depois acessar o valor do input
+    const workTime = workTimeInput.current?.value;
+    const shortBreakTime = shortBreakTimeInput.current?.value;
+    const longBreakTime = longBreakTimeInput.current?.value;
+    alert(
+      `Configurações salvas:\nFoco: ${workTime} minutos\nDescanso Curto: ${shortBreakTime} minutos\nDescanso Longo: ${longBreakTime} minutos`,
+    );
+  }
+
   return (
     <MainTemplate>
       <Container>
         <Heading>Configurações</Heading>
       </Container>
       <Container>
-        <p style={{ textAlign: 'center' }}>
-          Modifique as configurações para tempo de foco, <br/> descanço curto e descanço longo.
+        <p style={{ textAlign: "center" }}>
+          Modifique as configurações para tempo de foco,
+          <br />
+          descanço curto e descanço longo.
         </p>
       </Container>
 
       <Container>
-        <form action="" className="form">
+        <form onSubmit={handleSaveSettings} action="" className="form">
           <div className="formRow">
-            <DefaultInput id='workTime' labelText="Foco" type={"number"}/>
+            <DefaultInput
+              id="workTime"
+              labelText="Foco"
+              type={"number"}
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+            />
           </div>
 
           <div className="formRow">
-            <DefaultInput id='shortBreakTime' labelText="Descanso Curto" type={"number"}/>
+            <DefaultInput
+              id="shortBreakTime"
+              labelText="Descanso Curto"
+              type={"number"}
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+            />
           </div>
 
           <div className="formRow">
-            <DefaultInput id='longBreakTime' labelText="Descanso Longo" type={"number"}/>
+            <DefaultInput
+              id="longBreakTime"
+              labelText="Descanso Longo"
+              type={"number"}
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+            />
           </div>
 
           <div className="formRow">
-            <DefaultButton icon={<SaveIcon />} aria-label="Salvar Configurações" title="Salvar Configurações"/>
+            <DefaultButton
+              icon={<SaveIcon />}
+              aria-label="Salvar Configurações"
+              title="Salvar Configurações"
+            />
           </div>
         </form>
       </Container>
     </MainTemplate>
   );
 }
- 
